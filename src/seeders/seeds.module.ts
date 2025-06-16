d570a845a26e45a476dc44621fcff590531727fb
Rocio <rociobonindev@gmail.com>
2025-06-16 00:41:50 -0300

    refactor: reorganize database configuration and update email module to use async configuration

--- src/seeders/seeds.module.ts
+++ src/seeders/seeds.module.ts
@@ -4,13 +4,12 @@ import { User } from "src/modules/users/entities/user.entity";
 import { JwtModule } from "@nestjs/jwt";
 import { UsersSeeds } from "./user/user.seeds";
 import { Deposit } from "src/modules/deposit/entities/deposit.entity";
-import { DepositSeeds } from "./deposit/deposit.seeds";
 
 @Module({
     imports:[TypeOrmModule.forFeature([User, Deposit]),
     JwtModule,
 ],
-    providers: [UsersSeeds, DepositSeeds],
+    providers: [UsersSeeds],
     exports: [UsersSeeds],
 })
 
